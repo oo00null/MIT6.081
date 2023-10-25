1ef689a4e9fb6801423d73e47397114da4bb3e0d
211220000-Zhang San <zhangsan@foo.com>
2023-10-25 13:24:08 +0800

    #defind PTE_RSW

--- kernel/riscv.h
+++ kernel/riscv.h
@@ -353,6 +353,8 @@ r_fp()
 #define PTE_W (1L << 2)
 #define PTE_X (1L << 3)
 #define PTE_U (1L << 4) // 1 -> user can access
+//copy-on-write page
+#define PTE_RSW(1L << 8)
 
 // shift a physical address to the right place for a PTE.
 #define PA2PTE(pa) ((((uint64)pa) >> 12) << 10)
